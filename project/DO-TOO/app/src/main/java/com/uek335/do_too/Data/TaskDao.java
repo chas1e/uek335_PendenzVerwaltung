0d6976adab334c62d67ac447ed9916ff66a347e9
Chaste <pew.monstaaaa@gmail.com>
2021-05-27 05:40:16 +0200

    Infrastructure
    A little late sorry, this is the database, entity and dao

---
+++ project/DO-TOO/app/src/main/java/com/uek335/do_too/Data/TaskDao.java
@@ -0,0 +1,31 @@
+package com.uek335.do_too.Data;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Ignore;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.uek335.do_too.model.Task;
+
+import java.util.List;
+
+@Dao
+public interface TaskDao {
+
+    @Insert
+    void insertTask(Task task);
+
+    @Query("DELETE FROM task_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM task_table")
+    LiveData<List<Task>> getTasks();
+
+    @Query("SELECT * FROM task_table WHERE task_id ==:id")
+    LiveData<Task> get(long id);
+
+    @Delete
+    void delete(Task task);
+}
---
+++ project/DO-TOO/app/src/main/java/com/uek335/do_too/model/Priority.java
@@ -0,0 +1,8 @@
+package com.uek335.do_too.model;
+
+public enum Priority {
+    Niedrig,
+    Normal,
+    Hoch,
+    Abgelaufen
+}
